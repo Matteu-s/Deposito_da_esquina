cb27fe604104cc2990a77d1052f7797408d4b9cc
Mateus <mateeussantoos@hotmail.com>
2020-06-04 13:11:41 -0300

    Layout base quase ok, feita activity de whisky

---
+++ app/src/main/java/com/mateussantosti/deposito_da_esquina/produtos_whisky.java
@@ -0,0 +1,14 @@
+package com.mateussantosti.deposito_da_esquina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class produtos_whisky extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produtos_whisky);
+    }
+}
