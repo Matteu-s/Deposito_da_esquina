7364ef79d6f2f2c82c8890d2a1a9ee1ab8ed4267
Mateus <mateeussantoos@hotmail.com>
2020-06-04 00:44:22 -0300

    Adicionando imagens da tela inicial e configurando na tela.

--- app/src/main/java/com/mateussantosti/deposito_da_esquina/MainActivity.java
+++ app/src/main/java/com/mateussantosti/deposito_da_esquina/MainActivity.java
@@ -2,7 +2,10 @@ package com.mateussantosti.deposito_da_esquina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnRefrigerante = (Button)findViewById(R.id.btnRefrigerante);
+
+        btnRefrigerante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                telaRefrigerantes();
+            }
+        });
+    }
+    private void telaRefrigerantes() {
+        startActivity(new Intent(MainActivity.this, ProdutosRefrigerantes.class ));
     }
 }
---
+++ app/src/main/java/com/mateussantosti/deposito_da_esquina/ProdutosRefrigerantes.java
@@ -0,0 +1,14 @@
+package com.mateussantosti.deposito_da_esquina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProdutosRefrigerantes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produtos_refrigerantes);
+    }
+}
